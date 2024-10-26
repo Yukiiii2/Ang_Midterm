5e23029b893e785017eb8386b85c7d322a1a87a7
Yuki <142190110+Yukiiii2@users.noreply.github.com>
2024-10-26 23:05:27 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.24.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Earl\\OneDrive\\Pictures\\Ang_Console\\Ang_console_project\\console_project" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.24.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Earl\\OneDrive\\Pictures\\Ang_Console\\Ang_console_project\\console_project"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.24.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Earl\\OneDrive\\Pictures\\Ang_Console\\Ang_console_project\\console_project\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Earl\\OneDrive\\Pictures\\Ang_Console\\Ang_console_project\\console_project"
+  "FLUTTER_TARGET=C:\\Users\\Earl\\OneDrive\\Pictures\\Ang_Console\\Ang_console_project\\console_project\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Earl\\OneDrive\\Pictures\\Ang_Console\\Ang_console_project\\console_project\\.dart_tool\\package_config.json"
+)
